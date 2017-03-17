e4a5fb622905cb8e0e336f1516cc14cebe1ac683
newhouse <nhitbh@gmail.com>
2017-03-17 18:13:48 +0100

    Initial commit

---
+++ library/src/main/java/com/fooock/lib/phone/tracker/BaseService.java
@@ -0,0 +1,18 @@
+package com.fooock.lib.phone.tracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ *
+ */
+public class BaseService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
